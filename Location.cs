a545ef518371683e849b83180f6e854fed72339f
Franchi-21 <77765462+Franchi-21@users.noreply.github.com>
2022-10-05 20:49:31 -0300

    Create Location.cs

---
+++ Location.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeGame
+{
+    class Location
+    {
+        public int X { get; }
+        public int Y { get; }
+
+        public Location(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
